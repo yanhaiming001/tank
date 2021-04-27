6e0b31bd1e2415e8b17a84e57951123ec29ba505
yhm <1774072199@qq.com>
2021-04-28 06:21:24 +0800

    格式规范

--- tank/src/com/yhm/tank/TankFrame.java
+++ tank/src/com/yhm/tank/TankFrame.java
@@ -12,14 +12,15 @@ import java.awt.event.WindowEvent;
  */
 public class TankFrame extends Frame {
     //定义坐标值
-    int x=200;
-    int y=100;
+    int x = 200;
+    int y = 100;
+
     /**
      * 构造方法
      */
-    public TankFrame(){
+    public TankFrame() {
         //设置窗口的大小
-        this.setSize(800,600);
+        this.setSize(800, 600);
         //设置窗口不可更改窗口
         this.setResizable(false);
         //设置标题栏
@@ -43,14 +44,15 @@ public class TankFrame extends Frame {
 
     /**
      * 重写paint方法  绘制自己的Tank窗口，系统会自动调用
+     *
      * @param g
      */
     @Override
-    public void paint(Graphics g){
+    public void paint(Graphics g) {
         System.out.println("paint");
         //g是只画笔，可以在窗口中绘制图像
         //绘制一个正方形
-        g.fillRect(x,y,50,50);
+        g.fillRect(x, y, 50, 50);
 //        x+=10;
 //        y+=80;
         //paint被调用的时候，位置就会改变
@@ -61,7 +63,7 @@ public class TankFrame extends Frame {
      * 键盘监听处理类  内部类  只有这个类自己使用
      * 只处理键盘的监听
      */
-    class MyKeyListner extends KeyAdapter{
+    class MyKeyListner extends KeyAdapter {
         public MyKeyListner() {
             super();
         }
@@ -73,39 +75,40 @@ public class TankFrame extends Frame {
 
         /**
          * 监听按键 按下时调用
+         *
          * @param e
          */
         @Override
         public void keyPressed(KeyEvent e) {
             super.keyPressed(e);
-            x+=100;//按下任意一个按钮  x新增50
+            x += 100;//按下任意一个按钮  x新增50
 
             System.out.println("Key pressed");
+            int key = e.getKeyCode();//获得按键
 
-            int key=e.getKeyCode();//获得按键
-            switch (key){
-
-            case KeyEvent.VK_LEFT:
-                 boolean bL=true;
-                 break;
-            case KeyEvent.VK_RIGHT:
-                boolean bR=true;
-                break;
-            case KeyEvent.VK_UP:
-                boolean bU=true;
-                break;
-            case KeyEvent.VK_DOWN:
-                boolean bD=true;
-                break;
-            default:
-                break;
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+                    boolean bL = true;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    boolean bR = true;
+                    break;
+                case KeyEvent.VK_UP:
+                    boolean bU = true;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    boolean bD = true;
+                    break;
+                default:
+                    break;
             }
 
 
         }
+
         /*
-        * 按键被释放的时候调用
-        * */
+         * 按键被释放的时候调用
+         * */
         @Override
         public void keyReleased(KeyEvent e) {
             super.keyReleased(e);
